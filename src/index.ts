bac94991e76473e5e8f2f0cb3388d7deb5db1c51
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-01-28 11:03:23 -0600

    feat(loader): Use relative path inside SDL
    Use the file path instead of file content to import schema. Set Jest's environment to node
    to avoid the error with jsdom's localStorage, see facebook/jest#6766 and jsdom/jsdom#2304.
    
    BREAKING CHANGE: Use absolute path inside DSL won't work anymore, but now will be the same
    behavior of `graphql-import` dealing with paths.
    
    Fix prisma/graphql-import-loader#10

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { importSchema } from 'graphql-import'
 
 export default function(source) {
   const callback = this.async();
+  const isFileRegex = /\.graphql$/i
 
   this.cacheable()
 
-  callback(null, `module.exports = \`${importSchema(source).replace(/`/g, '\\`')}\``)
+  const sdl = importSchema(isFileRegex.test(this.resource) ? this.resource : source)
+
+  callback(null, `module.exports = \`${sdl.replace(/`/g, '\\`')}\``)
 }
